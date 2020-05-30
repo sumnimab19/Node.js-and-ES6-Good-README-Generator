31849006ea78f5df74d1ff1846344fa08bca20ed
Sumnima B <sumnimabhandari@gmail.com>
2020-05-29 22:54:30 -0700

    Added codes to fix the writeToFile and init functions

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Node.js-and-ES6-Good-README-Generator
+[![NPM Version](https://img.shields.io/npm/v/npm.svg?style=flat)]();
+  # ReadMe Generator
+  ## This application generates nice READ ME file based on user input.
+  ## Table of Contents
+    * [Installation](#install)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#needtoknowcontribute)
+    * [Tests](#runtest)
+    * [Questions](#needtoknowuse)
+    
+  ## Installation 
+  npm i
+
+  ## Usage 
+  undefined
+
+  ## License 
+  MIT
+
+  ## Contribution 
+  Users must know this and that
+
+  ## Tests 
+  npm test
+
+  ## Questions 
+  User should know this
+  
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,36 @@
 function generateMarkdown(data) {
-  return `
-# ${data.title}
 
-`;
+  return `[![NPM Version](https://img.shields.io/npm/v/npm.svg?style=flat)]();
+  # ${data.project}
+  ## ${data.description}
+  ## Table of Contents
+    * [Installation](#install)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#needtoknowcontribute)
+    * [Tests](#runtest)
+    * [Questions](#needtoknowuse)
+    
+  ## Installation 
+  ${data.install}
+
+  ## Usage 
+  ${data.usage}
+
+  ## License 
+  ${data.license}
+
+  ## Contribution 
+  ${data.needtoknowcontribute}
+
+  ## Tests 
+  ${data.runtest}
+
+  ## Questions 
+  ${data.needtoknowuse}
+  
+  `;
 }
 
 module.exports = generateMarkdown;
+
